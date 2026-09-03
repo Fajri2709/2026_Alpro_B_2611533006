cda19a3515783df33a5eed6a08ded6b67f4ae17b
Fajri2709 <fajriramadhan963@gmail.com>
2026-09-03 10:25:04 +0700

    Add files via upload

---
+++ Pekan1/Program1_2611533006.py
@@ -0,0 +1,12 @@
+# Program 1
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + " dibuat oleh a\n" + "Fajri Ramadhan dan 2611533006")
