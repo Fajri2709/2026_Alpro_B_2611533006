37add4807a335f42e170eef731765695a12ac28e
Fajri2709 <fajriramadhan963@gmail.com>
2026-09-10 17:38:13 +0700

    Add files via upload

---
+++ Pekan1/tugas1_2611533006.py
@@ -0,0 +1,11 @@
+print("=== Program Persegi Panjang ===")
+
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print("\nHasil Perhitungan")
+print("Luas      :", luas)
+print("Keliling  :", keliling)
